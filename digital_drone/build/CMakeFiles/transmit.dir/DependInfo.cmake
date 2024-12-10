3bb0201158195b3710fa280285cac8798ee70bec
samuelsm <samuelmeurer13@gmail.com>
2024-12-10 10:09:44 -0300

    Add digital_drone and radar_esp32

---
+++ digital_drone/build/CMakeFiles/transmit.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/ap_interface.c" "CMakeFiles/transmit.dir/ap_interface.c.o" "gcc" "CMakeFiles/transmit.dir/ap_interface.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/bluetooth.c" "CMakeFiles/transmit.dir/bluetooth.c.o" "gcc" "CMakeFiles/transmit.dir/bluetooth.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/bluez/lib/bluetooth.c" "CMakeFiles/transmit.dir/bluez/lib/bluetooth.c.o" "gcc" "CMakeFiles/transmit.dir/bluez/lib/bluetooth.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/bluez/lib/hci.c" "CMakeFiles/transmit.dir/bluez/lib/hci.c.o" "gcc" "CMakeFiles/transmit.dir/bluez/lib/hci.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/core-c/libopendroneid/opendroneid.c" "CMakeFiles/transmit.dir/core-c/libopendroneid/opendroneid.c.o" "gcc" "CMakeFiles/transmit.dir/core-c/libopendroneid/opendroneid.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/gpsmod.c" "CMakeFiles/transmit.dir/gpsmod.c.o" "gcc" "CMakeFiles/transmit.dir/gpsmod.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/common/cli.c" "CMakeFiles/transmit.dir/hostapd/src/common/cli.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/common/cli.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/common/wpa_ctrl.c" "CMakeFiles/transmit.dir/hostapd/src/common/wpa_ctrl.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/common/wpa_ctrl.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/utils/common.c" "CMakeFiles/transmit.dir/hostapd/src/utils/common.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/utils/common.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/utils/edit.c" "CMakeFiles/transmit.dir/hostapd/src/utils/edit.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/utils/edit.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/utils/eloop.c" "CMakeFiles/transmit.dir/hostapd/src/utils/eloop.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/utils/eloop.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/utils/os_unix.c" "CMakeFiles/transmit.dir/hostapd/src/utils/os_unix.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/utils/os_unix.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/hostapd/src/utils/wpa_debug.c" "CMakeFiles/transmit.dir/hostapd/src/utils/wpa_debug.c.o" "gcc" "CMakeFiles/transmit.dir/hostapd/src/utils/wpa_debug.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/print_bt_features.c" "CMakeFiles/transmit.dir/print_bt_features.c.o" "gcc" "CMakeFiles/transmit.dir/print_bt_features.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/transmit.c" "CMakeFiles/transmit.dir/transmit.c.o" "gcc" "CMakeFiles/transmit.dir/transmit.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/utils.c" "CMakeFiles/transmit.dir/utils.c.o" "gcc" "CMakeFiles/transmit.dir/utils.c.o.d"
+  "/home/samuelsm/skytrade/emitting/transmitter-linux/wifi_beacon.c" "CMakeFiles/transmit.dir/wifi_beacon.c.o" "gcc" "CMakeFiles/transmit.dir/wifi_beacon.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ digital_drone/build/CMakeFiles/transmit.dir/cmake_clean.cmake
@@ -0,0 +1,43 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transmit.dir/ap_interface.c.o"
+  "CMakeFiles/transmit.dir/ap_interface.c.o.d"
+  "CMakeFiles/transmit.dir/bluetooth.c.o"
+  "CMakeFiles/transmit.dir/bluetooth.c.o.d"
+  "CMakeFiles/transmit.dir/bluez/lib/bluetooth.c.o"
+  "CMakeFiles/transmit.dir/bluez/lib/bluetooth.c.o.d"
+  "CMakeFiles/transmit.dir/bluez/lib/hci.c.o"
+  "CMakeFiles/transmit.dir/bluez/lib/hci.c.o.d"
+  "CMakeFiles/transmit.dir/core-c/libopendroneid/opendroneid.c.o"
+  "CMakeFiles/transmit.dir/core-c/libopendroneid/opendroneid.c.o.d"
+  "CMakeFiles/transmit.dir/gpsmod.c.o"
+  "CMakeFiles/transmit.dir/gpsmod.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/common/cli.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/common/cli.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/common/wpa_ctrl.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/common/wpa_ctrl.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/common.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/common.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/edit.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/edit.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/eloop.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/eloop.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/os_unix.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/os_unix.c.o.d"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/wpa_debug.c.o"
+  "CMakeFiles/transmit.dir/hostapd/src/utils/wpa_debug.c.o.d"
+  "CMakeFiles/transmit.dir/print_bt_features.c.o"
+  "CMakeFiles/transmit.dir/print_bt_features.c.o.d"
+  "CMakeFiles/transmit.dir/transmit.c.o"
+  "CMakeFiles/transmit.dir/transmit.c.o.d"
+  "CMakeFiles/transmit.dir/utils.c.o"
+  "CMakeFiles/transmit.dir/utils.c.o.d"
+  "CMakeFiles/transmit.dir/wifi_beacon.c.o"
+  "CMakeFiles/transmit.dir/wifi_beacon.c.o.d"
+  "transmit"
+  "transmit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/transmit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
